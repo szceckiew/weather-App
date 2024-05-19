b28c70c44fb77f1edea977dc49abdac096b7d13c
szceckiew <115181486+szceckiew@users.noreply.github.com>
2024-05-19 16:29:51 +0200

    first commit

---
+++ weatherApp/views.py
@@ -0,0 +1,72 @@
+import requests
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.http import Http404, HttpResponse
+
+import jsonschema
+from jsonschema import validate
+
+import os
+from . import settings
+
+schema = {
+    "type": "object",
+    "properties": {
+        "latitude": {"type": "number", "minimum": -90, "maximum": 90},
+        "longitude": {"type": "number", "minimum": -180, "maximum": 180}
+    },
+    "required": ["latitude", "longitude"]
+}
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def get_img(request, filename):
+    img_dir = os.path.join(settings.STATICFILES_DIRS[0], 'img')
+    img_path = os.path.join(img_dir, filename)
+
+    if os.path.exists(img_path):
+        with open(img_path, 'rb') as f:
+            return HttpResponse(f.read(), content_type='image/png')
+    else:
+        raise Http404('Image not found')
+
+def get_weather_data(request, latitude, longitude):
+    lat_long = {"latitude": latitude, "longitude": longitude}
+    try:
+        validate(instance=lat_long, schema=schema)
+    
+        response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=" + str(latitude) + "&longitude=" + str(longitude) + "&daily=weather_code,apparent_temperature_max,apparent_temperature_min,sunshine_duration&timezone=auto").json()
+
+        response_daily = response['daily']
+
+        generatedEnergy = []
+
+        installationPower = 2.5
+        effectivness = 0.2
+
+        for day in response_daily['sunshine_duration']:
+            print(day / 3600 * installationPower * effectivness)
+            print(round(day / 3600 * installationPower * effectivness))
+
+            generatedEnergy.append(round(day / 3600 * installationPower * effectivness, 2))
+
+        json_data = {}
+
+        json_data['date'] = response_daily['time']
+        json_data['weather_code'] = response_daily['weather_code']
+        json_data['min_temp'] = response_daily['apparent_temperature_min']
+        json_data['max_temp'] = response_daily['apparent_temperature_max']
+        json_data['gen_energy'] = generatedEnergy
+
+        return JsonResponse(json_data)
+
+    except jsonschema.exceptions.ValidationError as e:
+        
+        json_data ={}
+        json_data['error'] = str(e)
+        
+        return JsonResponse(json_data)
+        
+
